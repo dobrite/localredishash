818f81b06e0103295c280e32d67654d31a437f80
Dave O <davidobrite@gmail.com>
2014-05-09 20:24:56 -0700

    lil specs

--- spec/localredishash_spec.rb
+++ spec/localredishash_spec.rb
@@ -2,6 +2,8 @@ require 'localredishash'
 
 describe LocalRedisHash, '#initialize' do
   let(:hash) { LocalRedisHash.new }
+  subject { hash }
+  it { should respond_to :[], :use }
   it 'thinks therefore it is' do
     expect(hash).to be_an_instance_of LocalRedisHash
   end
